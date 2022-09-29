d241bf11fe8601f2938f6a6cc20da6a4e19c6b83
Shamsrary@gmail.com <Shamsrary@gmail.com>
2022-09-29 12:32:09 -0400

    mid way

---
+++ src/Tips.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Tips {
+    public static void main(String[] args)
+    {
+        System.out.println("Welocome user type in your bill.");
+        Scanner input = new Scanner(System.in);
+
+        double totalBill = input.nextDouble();
+
+        System.out.println("Type in your Tip Percentage.");
+        double tipPercentage = input.nextDouble();
+
+        System.out.println("Type in your the amount of people in your party.");
+        int totalpeople = input.nextInt();
+
+
+        double totaltip = tipPercentage * totalBill;
+        double totalCost =
+
+
+
+
+
+
+    }
+}
